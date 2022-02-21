761751148f585a95888ee10794ef370180ec6f22
Alexandre Halley <alexandre.m.halley@gmail.com>
2022-02-21 17:37:24 +0100

    alex as a driver : iteration 3

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 // Iteration 1: Names and Input
-const hacker1 = 'Noa';
-const hacker2 = 'Anh AND Alex';
-console.log(`The driver's name is ${hacker1}`);
-console.log(`The navigator's names are ${hacker2}`);
+const hacker1 = 'Noa'
+const hacker2 = 'Anh AND Alex'
+console.log(`The driver's name is ${hacker1}`)
+console.log(`The navigator's names are ${hacker2}`)
 
 
 // Iteration 2: Conditionals
@@ -15,3 +15,34 @@ if(hacker1.length > hacker2.length){
 }
 
 // Iteration 3: Loops
+let nameCapital = ''
+for (let i=0; i< hacker1.length; i++){
+    nameCapital += `${hacker1[i].toUpperCase()}`
+    if (i===hacker1.length-1){
+        continue
+    }
+    nameCapital += ' '
+}
+console.log(nameCapital)
+
+let nameReversed = ''
+for ( let i = hacker2.length-1; i>= 0; i--){
+    nameReversed += hacker2[i]
+}
+console.log(nameReversed)
+
+
+switch(hacker1.localeCompare(hacker2)){
+    case 1:
+        console.log("Yo, the navigator goes first definitely.")
+        break
+    case 0:
+        console.log("What?! You both have the same name?")
+        break
+    case -1:
+        console.log("The driver's name goes first.")
+        break
+}
+
+
+
