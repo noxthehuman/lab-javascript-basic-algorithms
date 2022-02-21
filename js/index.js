abd7ba0cb629e9b13c418d14eeeb7a806fac6e00
Nox <benaziz.ines@gmail.com>
2022-02-21 17:06:39 +0100

    Itération 1 faite par Noa

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 // Iteration 1: Names and Input
-console.log("test");
+const hacker1 = 'noa';
+const hacker2 = 'Anh and Alex';
+console.log(`The driver's name is ${hacker1}`);
+console.log(`The navigator's names are ${hacker2}`);
+
+
+
 // Iteration 2: Conditionals
 
 
